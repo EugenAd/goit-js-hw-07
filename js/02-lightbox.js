c1a1ad25fa368d5c3fa0c6f00222c451871d5979
EugenAd <bosmaneugen@gmail.com>
2024-01-13 20:06:24 +0200

    Task 02-lightbox completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ulElem = document.querySelector(".gallery");
+galleryItems.forEach((item) => {
+  const liElem = document.createElement("li");
+  liElem.classList.add("gallery__item");
+  const aElem = document.createElement("a");
+  aElem.href = item.original;
+  aElem.classList.add("gallery__link");
+  aElem.dataset.lightbox = "image";
+  const imageElement = document.createElement("img");
+  imageElement.classList.add("gallery__image");
+  imageElement.src = item.preview;
+  imageElement.alt = item.description;
+  aElem.appendChild(imageElement);
+  liElem.appendChild(aElem);
 
-console.log(galleryItems);
+  const dFrag = document.createDocumentFragment();
+  dFrag.appendChild(liElem);
+  ulElem.append(dFrag);
+});
+let gallery = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+ulElem.addEventListener("click", (e) => {
+  if (e.target.classList.contains("gallery__image")) {
+    e.preventDefault();
+  }
+
+  const imageElement = e.target;
+
+  gallery.show(imageElement);
+});
